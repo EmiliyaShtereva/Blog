06c13029b1bef96afc220fc0939e1f1292842304
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-04-19 22:35:02 +0300

    generated secret

--- blog/src/config.js
+++ blog/src/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
     PORT: 3000,
-    DB_URL: "mongodb://localhost:27017/blog"
+    DB_URL: "mongodb://localhost:27017/blog",
+    SECRET: "a19e5606-d707-48f0-98d4-98b6b0909e61"
 };
 
 module.exports = CONFIG;
