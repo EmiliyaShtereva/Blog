986f57397c490ff0f6326a16d90ac2bc6e96722b
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-05-07 22:34:10 +0300

    change db url

--- blog/src/config.js
+++ blog/src/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     PORT: 3000,
-    DB_URL: "mongodb://localhost:27017/blog",
+    DB_URL: "mongodb://127.0.0.1:27017/blog",
     SECRET: "a19e5606-d707-48f0-98d4-98b6b0909e61"
 };
 
